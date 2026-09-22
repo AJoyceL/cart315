8d8bdbaa197d6838b85aaa14fa1299db18f14137
AJoyceL <LamAJoyc3@gmail.com>
2026-09-22 13:57:43 -0400

    made the ball invisible when collision with paddle
    - Turning the ball invisible after collision creates confusion  because the player loses sight of the ball.
    
    - ref: https://discussions.unity.com/t/changing-alpha-of-2d-game-object-when-collided-with/1536496/3
    
    - invoke line from Court.cs

--- P1_Pong/Assets/Scripts/Ball.cs
+++ P1_Pong/Assets/Scripts/Ball.cs
@@ -7,10 +7,27 @@ public class Ball : MonoBehaviour
     private Rigidbody2D _rigidBody;
 
     public float speed = 100.0f;
+    
+    //transparency variables
+    public float alpha = 1f;
+    public float fadeSpeed = 10.0f;
+    private float duration = 5.0f;
+    SpriteRenderer ballSprite;
+    Color ballColor;
+
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        
+        //Calls the ball spirerender + colour
+        ballSprite = GetComponent<SpriteRenderer>();
+        ballColor = Color.white;
+    }
+
+    private void Update()
+    {
+        ballSprite.color = Color.Lerp(ballSprite.color, ballColor, fadeSpeed * Time.deltaTime);
     }
 
     public void ResetBall()
@@ -28,5 +45,21 @@ public class Ball : MonoBehaviour
         Vector2 direction = new Vector2(x, y);
 
         _rigidBody.AddForce(direction * speed);
+    }
+    
+    //triggers transparency when ball collides with paddle
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Paddle>() != null)
+        {
+            ballColor = new Color(1, 1, 1, 0);
+            Invoke(nameof(MakeVisible), duration);
+        }
+    }
+
+    //calls back the ball to visible
+    private void MakeVisible()
+    {
+        ballColor = new Color(1, 1, 1, alpha);
     }
 }
